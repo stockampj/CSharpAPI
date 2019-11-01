cdf59c26dfd0f73407513ae2f430c8159dace8ea
Joel Stockamp <stockampj@gmail.com>
2019-11-01 11:28:19 -0700

    add routes for trails. Search queries can be limitted by hike length

---
+++ ParksAPI/Controllers/TrailsController.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ParksAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ParksAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrailsController : ControllerBase
+    {
+        private ApplicationDbContext _db;
+
+        public TrailsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// You may enter a String with a Park ID or a length minimum to filter your searches.
+        /// </summary>
+        [HttpGet]
+        public ActionResult<IEnumerable<Trail>> Get(string parkId, string lengthMin, string lengthMax, string trailId)
+        {
+            var query = _db.Trails.AsQueryable();
+
+            if(parkId != null)
+            {
+            int parkIdInt = Int32.Parse(parkId);
+            query = query
+                .Where(trail => trail.ParkId == parkIdInt);
+            }
+
+            if(lengthMin != null)
+            {
+            int lengthMinInt = Int32.Parse(lengthMin);
+            query = query
+                .Where(trail => trail.Length >= lengthMinInt);
+            }
+
+            if(lengthMax != null)
+            {
+            int lengthMaxInt = Int32.Parse(lengthMax);
+            query = query
+                .Where(trail => trail.Length >= lengthMaxInt);
+            }
+
+            if( trailId != null)
+            {
+            int trailIdint = Int32.Parse(trailId);
+            query = query
+                .Where(trail => trail.TrailId == trailIdint);
+            }
+
+            return query.ToList();
+        }
+        // POST api/animals
+        [HttpPost]
+        public void Post([FromBody] Trail trail)
+        {
+            _db.Trails.Add(trail);
+            _db.SaveChanges();
+
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Trail trail)
+        {
+            trail.TrailId = id;
+            _db.Entry(trail).State = EntityState.Modified;
+            _db.SaveChanges();
+
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var trailToDelete = _db.Trails.FirstOrDefault(entry => entry.TrailId == id);
+            _db.Trails.Remove(trailToDelete);
+            _db.SaveChanges();
+        }
+    }
+}
