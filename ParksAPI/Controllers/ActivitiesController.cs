5d4ebb597ca8a624bc4bc10cccb0a48d3b2ff940
Joel Stockamp <stockampj@gmail.com>
2019-11-01 10:27:29 -0700

    add CRUD for the Park controller

---
+++ ParksAPI/Controllers/ActivitiesController.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ParksAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ParksAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParksController : ControllerBase
+    {
+        private ApplicationDbContext _db;
+
+        public ParksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Park>> Get(string parkName)
+        {
+            var query = _db.Parks.AsQueryable();
+
+            if(parkName != null)
+            {
+              query = query
+                // .Include(park => park.Trails)
+                .Where(entry => entry.ParkName.ToLower().Replace(" ", "") == parkName.ToLower().Replace(" ", ""));
+            }
+
+            return query.ToList();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Park park)
+        {
+
+            _db.Parks.Add(park);
+            _db.SaveChanges();
+            
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Park park)
+        {
+            park.ParkId = id;
+            _db.Entry(park).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+            _db.Parks.Remove(parkToDelete);
+            _db.SaveChanges();
+        }
+    }
+}
